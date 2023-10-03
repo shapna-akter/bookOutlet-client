29a134cf6bd5f27c4adf1c35c0cb2327a397fda6
shapna28 <shapna088@gmail.com>
2023-10-03 19:54:27 +0600

    added server link to the client

--- src/shared/mainApi.tsx
+++ src/shared/mainApi.tsx
@@ -1,3 +1,5 @@
-const baseUrl = "http://localhost:5000/api/v1";
+// const baseUrl = "http://localhost:5000/api/v1";
+
+const baseUrl = "https://book-outlet-server.vercel.app/api/v1";
 
 export const env = { baseUrl };
