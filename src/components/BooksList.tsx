c7e713ee8e95e4a41abb96b11f2b757d9f8611ba
shapna28 <shapna088@gmail.com>
2023-10-04 03:23:57 +0600

    all new book page added

--- src/components/BooksList.tsx
+++ src/components/BooksList.tsx
@@ -34,53 +34,53 @@ const Books = () => {
   };
 
   return (
-    <div className="text-center py-8">
+    <div className="container mx-auto px-4 py-8 max-w-5xl text-center">
       <h2 className="text-3xl text-primary font-bold mb-4 text-center">
         All New Books
       </h2>
       {isLoading && <Spinner></Spinner>}
-      <div className="grid  lg:grid-cols-3 gap-7 sm:max-w-sm sm:mx-auto lg:max-w-full mt-5">
+      <div className="grid gap-5 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
         {books?.map((book, i) => {
           const { _id, title, author, genre, publicationDate } = book;
           return (
             <div
               key={i + 1}
-              className="flex flex-wrap justify-center my-2  gap-3 shadow-md hover:shadow-1xl hover:scale-105  text-center"
+              className="flex flex-wrap -mx-4 shadow-md hover:shadow-2xl hover:scale-105 py-1 text-center"
             >
-              <Link to={`/bookDetails/${_id}`} className="w-full  px-4 my-1 ">
-                <div className="card w-96 bg-base-100 shadow-xl image-full">
+              <Link to={`/bookDetails/${_id}`} className="w-full  px-4 my-1">
+                <div className="card bg-white shadow-xl image-full">
                   <figure>
                     <img
                       src="https://thumbs.dreamstime.com/b/open-old-hardcover-book-light-blue-background-open-old-hardcover-book-light-blue-background-166398262.jpg"
-                      alt="Shoes"
+                      alt="Book"
                     />
                   </figure>
                   <div className="card-body">
                     <div className="mx-auto text-center">
-                      <h2 className=" text-lg font-bold mb-2">{title}</h2>
-                      <p className="">{author}</p>
-                      <p className="">{genre}</p>
-                      <p className="">Publication Date: {publicationDate}</p>
+                      <h2 className="text-lg font-bold mb-2">{title}</h2>
+                      <p>{author}</p>
+                      <p>{genre}</p>
+                      <p>Publication Date: {publicationDate}</p>
                     </div>
 
                     <div className="card-actions justify-end">
                       {user?.email && (
-                        <section className="mx-auto  grid grid-cols-3 lg:grid-cols-3 gap-3 text-sm font-medium">
+                        <section className="grid grid-cols-3 lg:grid-cols-3 gap-3 text-sm font-medium">
                           <Link
                             to={`/bookDetails/${_id}`}
-                            className="py-2 bg-gray-200 text-purple-800 font-bold rounded-sm my-2 px-1"
+                            className="py-2 bg-gray-200 text-info font-bold rounded-sm my-2 px-1"
                           >
                             See Details
                           </Link>
                           <button
                             onClick={() => handleAddBook(book)}
-                            className="py-2 bg-gray-200 text-purple-800 font-bold  my-2 px-1 rounded-sm"
+                            className="py-2 bg-gray-200 text-info font-bold my-2 px-1 rounded-sm"
                           >
                             Add Reading
                           </button>
                           <button
                             onClick={() => handleAddReaded(book)}
-                            className="py-2 bg-gray-200 text-purple-800 font-bold  rounded-sm my-2 px-1"
+                            className="py-2 bg-gray-200 text-info font-bold rounded-sm my-2 px-1"
                           >
                             Add Finished
                           </button>
